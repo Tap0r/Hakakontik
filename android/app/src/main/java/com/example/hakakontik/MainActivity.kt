0fddc0d73ddbfb4dc3737d2b728afd3bc22b4889
Tap0r <88531962+Tap0r@users.noreply.github.com>
2025-02-19 20:14:32 +0300

    зачаток тач листенера

--- android/app/src/main/java/com/example/hakakontik/MainActivity.kt
+++ android/app/src/main/java/com/example/hakakontik/MainActivity.kt
@@ -1,21 +1,13 @@
 package com.example.hakakontik
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hakakontik.ui.theme.HakakontikTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        findViewById<View>(R.id.touchlistener).setOnTouchListener(TouchListener(findViewById(R.id.scrolls)))
     }
 }
