547645ff5e40eb8b7dc0a79ddb6d98c9d959012b
Tap0r <88531962+Tap0r@users.noreply.github.com>
2025-02-20 00:26:03 +0300

    свайпы фрагменты база коре

--- android/app/src/main/java/com/example/hakakontik/MainActivity.kt
+++ android/app/src/main/java/com/example/hakakontik/MainActivity.kt
@@ -2,12 +2,34 @@ package com.example.hakakontik
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.ComponentActivity
+import android.widget.ImageButton
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.hakakontik.fragments.ViewPagerAdapter
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : FragmentActivity() {
+    private val viewPager: ViewPager2 by lazy { findViewById(R.id.viewpager) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        findViewById<View>(R.id.touchlistener).setOnTouchListener(TouchListener(findViewById(R.id.scrolls)))
+
+        viewPager.adapter = ViewPagerAdapter(this)
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                when(position){
+                    0 -> findViewById<ImageButton>(R.id.olympiads)
+                    1 -> findViewById<ImageButton>(R.id.olympiads)
+                    2 -> findViewById<ImageButton>(R.id.olympiads)
+                }
+            }
+        })
     }
+
+    fun onOlympClick(view: View?) { viewPager.currentItem = 0 }
+    fun onEventClick(view: View?) { viewPager.currentItem = 1 }
+    fun onAssocClick(view: View?) { viewPager.currentItem = 2 }
 }
