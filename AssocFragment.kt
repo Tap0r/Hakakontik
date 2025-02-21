a16015623b2bb0dddc3781767ad06bd0fd3d9150
Tap0r <88531962+Tap0r@users.noreply.github.com>
2025-02-21 18:03:45 +0300

    Revert "Revert "все кроме кода""

---
+++ AssocFragment.kt
@@ -0,0 +1,53 @@
+package com.example.hakakontik.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.hakakontik.R
+import com.example.hakakontik.databinding.AssociationsBinding
+import com.example.hakakontik.databinding.OlympiadsBinding
+
+
+class AssocFragment: Fragment() {
+
+
+    private var _binding: AssociationsBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("_binding is null")
+    lateinit var listLv: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = AssociationsBinding.inflate(layoutInflater)
+
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.associations, container, false)
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "as1", "as2", "as3", "as4", "as5", "as6", "as7", "as8",
+            "as9", "as10", "as11", "as12", "as13", "as14", "as15", "as16"
+        )
+
+        // access the listView from xml file
+        listLv = view.findViewById(R.id.AssList)
+        arrayAdapter = ArrayAdapter(requireContext(), R.layout.list_item_style, users)
+        listLv.adapter = arrayAdapter
+        listLv.setOnItemClickListener { parent, view, position, id ->
+
+            Toast.makeText(requireContext(), parent.getPositionForView(view).toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        return view
+    }
+
+
+
+
+
+    }
