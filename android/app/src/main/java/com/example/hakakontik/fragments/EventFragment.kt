dcf6d516312c43db2857d4bdea5e2708c080cc0f
Tap0r <88531962+Tap0r@users.noreply.github.com>
2025-02-21 17:55:29 +0300

    Revert "все кроме кода"

--- android/app/src/main/java/com/example/hakakontik/fragments/EventFragment.kt
+++ android/app/src/main/java/com/example/hakakontik/fragments/EventFragment.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.hakakontik.R
-import com.example.hakakontik.databinding.EventsBinding
-import com.example.hakakontik.databinding.OlympiadsBinding
 
 
 class EventFragmentNavHost(): Fragment() {
@@ -20,34 +15,8 @@ class EventFragmentNavHost(): Fragment() {
 }
 
 
-class EventFragment: Fragment() {
-    private var _binding: EventsBinding? = null
-    private val binding
-        get() = _binding ?: throw IllegalStateException("_binding is null")
-    lateinit var listLv: ListView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        _binding = EventsBinding.inflate(layoutInflater)
-    }
-
+class EventFragment(): Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.events, container, false)
-        val arrayAdapter: ArrayAdapter<*>
-        val users = arrayOf(
-            "ev1", "ev2", "ev3", "ev4", "ev5", "ev6", "ev7", "ev8",
-            "ev9", "ev10", "ev11", "ev12", "ev13", "ev14", "ev15", "ev16"
-        )
-
-        // access the listView from xml file
-        listLv = view.findViewById(R.id.NewsList)
-        arrayAdapter = ArrayAdapter(requireContext(), R.layout.list_item_style, users)
-        listLv.adapter = arrayAdapter
-        listLv.setOnItemClickListener { parent, view, position, id ->
-
-            Toast.makeText(requireContext(), parent.getPositionForView(view).toString(), Toast.LENGTH_SHORT).show()
-        }
-
-        return view
+        return inflater.inflate(R.layout.events, container, false)
     }
 }
